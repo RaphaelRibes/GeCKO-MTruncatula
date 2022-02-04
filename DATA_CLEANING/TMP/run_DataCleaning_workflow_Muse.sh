9399610ff479b1de6a4fac587ef99fa366b08717
Johanna Girodolle <johanna.girodolle@gmail.com>
2022-02-04 15:40:46 +0100

    cluster config jason 2

--- DATA_CLEANING/TMP/run_DataCleaning_workflow_Muse.sh
+++ DATA_CLEANING/TMP/run_DataCleaning_workflow_Muse.sh
@@ -17,23 +17,23 @@ CONFIG=/home/girodollej/scratch/DATA_TEST/VIR_Cap001/CONFIG
 
 
 ## DRYRUN ##
-#snakemake --snakefile ${PRGR}/DataCleaning.smk -n --dag --forceall --cluster-config ${CONFIG}/cluster_config.yml --configfile ${CONFIG}/config.yml --printshellcmds
+#snakemake --snakefile ${PRGR}/DataCleaning.smk -n --dag --forceall --cluster-config ${CONFIG}/cluster_config.json --configfile ${CONFIG}/config.yml --printshellcmds
 
 ## GENERATE PIPELINE DIAGRAM -> à lancer hors sbatch, dot est indisponible sur les noeuds de calcul ##
-#snakemake --snakefile ${PRGR}/DataCleaning.smk -n --dag --forceall --cluster-config ${CONFIG}/cluster_config.yml --configfile ${CONFIG}/mini_tmp/config_mini.yml | dot -Tsvg > data_cleaning_pipeline_mini.svg
+#snakemake --snakefile ${PRGR}/DataCleaning.smk -n --dag --forceall --cluster-config ${CONFIG}/cluster_config.json --configfile ${CONFIG}/mini_tmp/config_mini.yml | dot -Tsvg > data_cleaning_pipeline_mini.svg
 
 
 
                                         ### CLASSIC CLUSTER CONFIG ###
 
 ## RUN WITH ENVMODULES ##
-#snakemake --snakefile ${PRGR}/DataCleaning.smk --cluster-config ${CONFIG}/cluster_config.yml --configfile ${CONFIG}/config.yml --jobs 200 --printshellcmds --use-envmodules --cluster "sbatch -p {cluster.partition} --job-name {cluster.job-name} -e {cluster.error} -o {cluster.output}"
+#snakemake --snakefile ${PRGR}/DataCleaning.smk --cluster-config ${CONFIG}/cluster_config.json --configfile ${CONFIG}/config.yml --jobs 200 --printshellcmds --use-envmodules --cluster "sbatch -p {cluster.partition} --job-name {cluster.job-name} -e {cluster.error} -o {cluster.output}"
 
 ## RUN WITH CONDA ##
-snakemake --snakefile ${PRGR}/DataCleaning.smk --cluster-config ${CONFIG}/cluster_config.yml --configfile ${CONFIG}/config.yml --jobs 200 --printshellcmds --use-conda --cluster "sbatch -p {cluster.partition} --job-name {cluster.job-name} -e {cluster.error} -o {cluster.output}"
+snakemake --snakefile ${PRGR}/DataCleaning.smk --cluster-config ${CONFIG}/cluster_config.json --configfile ${CONFIG}/config.yml --jobs 200 --printshellcmds --use-conda --cluster "sbatch -p {cluster.partition} --job-name {cluster.job-name} -e {cluster.error} -o {cluster.output}"
 
 ## GENERATE PIPELINE REPORT ##
-#snakemake --snakefile ${PRGR}/DataCleaning.smk --cluster-config ${CONFIG}/cluster_config.yml --configfile ${CONFIG}/config.yml --jobs 200 --printshellcmds --use-conda --report Snakemake_Report_DataCleaning.html --cluster "sbatch -p {cluster.partition} --job-name {cluster.job-name} -e {cluster.error} -o {cluster.output}"
+#snakemake --snakefile ${PRGR}/DataCleaning.smk --cluster-config ${CONFIG}/cluster_config.json --configfile ${CONFIG}/config.yml --jobs 200 --printshellcmds --use-conda --report Snakemake_Report_DataCleaning.html --cluster "sbatch -p {cluster.partition} --job-name {cluster.job-name} -e {cluster.error} -o {cluster.output}"
 
 
                                   ### CLUSTER CONFIG MAKING USE OF PROFILES ###
