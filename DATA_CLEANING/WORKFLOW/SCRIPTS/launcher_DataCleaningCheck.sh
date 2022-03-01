aad243576b4764a3089daf0e79bac19653a60563
Johanna Girodolle <johanna.girodolle@gmail.com>
2022-03-01 16:01:31 +0100

    Updated runSnakemakeWorkflow.sh

---
+++ DATA_CLEANING/WORKFLOW/SCRIPTS/launcher_DataCleaningCheck.sh
@@ -0,0 +1,242 @@
+#!/bin/bash
+
+# Model config file
+if [[ "$WORKFLOW_SMK" = "${WORKFLOW}_PairedEnd.smk" ]] ; then
+  echo -e "\nINFO: Paired end data is expected (PAIRED_END set to TRUE)\n"
+  model_config="${WORKFLOW_PATH}/SCRIPTS/model_files/config_DataCleaning_PE.yml"
+else
+  echo -e "\nINFO: Single end data is expected (PAIRED_END set to FALSE)\n"
+  model_config="${WORKFLOW_PATH}/SCRIPTS/model_files/config_DataCleaning_SE.yml"
+fi
+
+
+# Config file variables
+given_variables=$(grep -v '^#' $CONFIG | grep -v '^$' | cut -f1 -d ' ')
+expected_variables=$(grep -v '^#' $model_config | grep -v '^$' | cut -f1 -d ' ')
+
+for var in $expected_variables ; do
+  var_in_config=$(grep "^$var " $CONFIG)
+  if [[ -z $var_in_config ]] ; then
+    echo -e "\nERROR: The expected variable $var was not found in your config file (${CONFIG}). Please make sure to include it."
+    echo -e "\nList of expected variables for PAIRED_END set to ${PAIRED_END} (some can be left empty but must appear in the file nonetheless) :\n${expected_variables}"
+    echo -e "\nAs a reminder:"
+    echo "All expected variables must be specified in YAML format, with one variable per row followed by a ':' and a space before its assigned value. For example: VARIABLE_NAME: value."
+    echo "To leave a variable empty, use quotes: VARIABLE_NAME: \"\" "
+    echo -e "\nExiting.\n"
+    exit 1
+  fi
+done
+
+
+### Config file variables values
+
+# OUTPUTS_DIRNAME
+OUTPUTS_DIRNAME=$(grep "^OUTPUTS_DIRNAME:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+if [[ -z $OUTPUTS_DIRNAME ]] ; then
+  echo -e "\nERROR: You must provide the OUTPUTS_DIRNAME in the config_file."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+
+
+# FASTQ, FASTQ_R1, FASTQ_R2 and DEMULT_DIR
+FASTQ_R1=$(grep "^FASTQ_R1:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+FASTQ_R2=$(grep "^FASTQ_R2:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+DEMULT_DIR=$(grep "^DEMULT_DIR:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+FASTQ=$(grep "^FASTQ:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+FASTQ_R1=$(absolutePath $FASTQ_R1)
+FASTQ_R2=$(absolutePath $FASTQ_R2)
+DEMULT_DIR=$(absolutePath $DEMULT_DIR)
+FASTQ=$(absolutePath $FASTQ)
+
+
+if [[ "$WORKFLOW_SMK" = "${WORKFLOW}_PairedEnd.smk" ]] ; then # if paired end data is expected
+  if [[ (! -z "$FASTQ_R1" || ! -z "$FASTQ_R2") && ! -z "$DEMULT_DIR" ]] ; then
+    echo -e "\nERROR: You provided both FASTQ_R1 and/or FASTQ_R2 and DEMULT_DIR in the config_file. Please provide either FASTQ_R1 and FASTQ_R2 in case of multiplexed data, or DEMULT_DIR in case of demultiplexed data."
+    echo -e "\nExiting.\n"
+    exit 1
+  fi
+  if [[ -z "$FASTQ_R1" || -z "$FASTQ_R2" ]] ; then
+    if [[ -z "$DEMULT_DIR" ]] ; then
+      echo -e "\nERROR: You must either provide FASTQ_R1 and FASTQ_R2 or DEMULT_DIR in the config_file, but they were all left blank."
+      echo -e "\nExiting.\n"
+      exit 1
+    elif [[ ! -d "$DEMULT_DIR" ]] ; then
+      echo -e "\nERROR: the DEMULT_DIR parameter provided in the config file (${DEMULT_DIR}) is not a valid path. Please make sure the directory exists and the path is correctly written."
+      echo -e "\nExiting.\n"
+      exit 1
+    else
+      nb_fastq=$(ls ${DEMULT_DIR}/*fastq.gz 2>/dev/null | wc -l)
+      if [[ "$nb_fastq" = 0 ]] ; then
+        echo -e "\nERROR: The provided DEMULT_DIR (${DEMULT_DIR}) is either empty or the fastq files it contains are not properly named. Input demultiplexed fastq files must end with '.fastq.gz'."
+        echo -e "\nExiting.\n"
+        exit 1
+      else
+        fastq_R1_list=$(ls -1 ${DEMULT_DIR}/*R1.fastq.gz 2>/dev/null | xargs -n1 basename)
+        nb_fastq_R2_obs=$(ls ${DEMULT_DIR}/*R2.fastq.gz 2>/dev/null | wc -l)
+        fastq_R2_list_exp=$(echo $fastq_R1_list | sed 's/.R1./.R2./g')
+        nb_fastq_R2_exp=$(echo $fastq_R2_list_exp | wc -w)
+        for fastq_r2 in $fastq_R2_list_exp ; do
+          if [[ ! -f ${DEMULT_DIR}/$fastq_r2 || $nb_fastq_R2_obs != $nb_fastq_R2_exp ]] ; then
+            echo -e "\nERROR: R1 and R2 fastq files provided in the config file do not seem to match. A set of matching *.R1.fastq.gz and *.R2.fastq.gz are expected in the DEMULT folder you provided (${DEMULT_DIR})."
+            echo -e "\nExiting.\n"
+            exit 1
+          fi
+        done
+      fi
+    fi
+  elif [[ ! -f "$FASTQ_R1" || ! -f "$FASTQ_R2" ]] ; then
+    echo -e "\nERROR: At least one the two input fastq files ${FASTQ_R1} and ${FASTQ_R2} provided in the config file does not exist. Please make sure the files exist and their path is correct."
+    echo -e "\nExiting.\n"
+    exit 1
+  elif [[ "$FASTQ_R1" != *_R1.fastq.gz || "$FASTQ_R2" != *_R2.fastq.gz ]] ; then
+      echo -e "\nERROR: Provided FASTQ_R1 and FASTQ_R2 (${FASTQ_R1} and ${FASTQ_R2}) are not properly named. Multiplexed fastq files must end with _R1.fastq.gz and _R2.fastq.gz."
+      echo -e "\nExiting.\n"
+      exit 1
+  else
+    R1_basename=$(basename $FASTQ_R1 _R1.fastq.gz)
+    R2_basename=$(basename $FASTQ_R2 _R2.fastq.gz)
+    if [[ "$R1_basename" != "$R2_basename" ]] ; then
+      echo -e "\nERROR: Provided FASTQ_R1 and FASTQ_R2 names do not match: ${R1_basename} != ${R2_basename}."
+      echo -e "\nExiting.\n"
+      exit 1
+    fi
+  fi
+else # if single end data is expected
+  if [[ ! -z "$FASTQ" && ! -z "$DEMULT_DIR" ]] ; then
+    echo -e "\nERROR: You provided both FASTQ and DEMULT_DIR in the config_file. Please provide either FASTQ in case of multiplexed data, or DEMULT_DIR in case of demultiplexed data."
+    echo -e "\nExiting.\n"
+    exit 1
+  fi
+  if [[ -z "$FASTQ" ]] ; then
+    DEMULT_DIR=$(grep "^DEMULT_DIR:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+    if [[ -z "$DEMULT_DIR" ]] ; then
+      echo -e "\nERROR: You must provide either FASTQ or DEMULT_DIR in the config_file, but they were both left blank."
+      echo -e "\nExiting.\n"
+      exit 1
+    elif [[ ! -d "$DEMULT_DIR" ]] ; then
+      echo -e "\nERROR: the DEMULT_DIR parameter provided in the config file (${DEMULT_DIR}) is not a valid path. Please make sure the directory exists and the path is correctly written."
+      echo -e "\nExiting.\n"
+      exit 1
+    else
+      nb_fastq=$(ls ${DEMULT_DIR}/*fastq.gz 2>/dev/null | wc -l)
+      if [[ "$nb_fastq" = 0 ]] ; then
+        echo -e "\nERROR: The provided DEMULT_DIR (${DEMULT_DIR}) is either empty or the fastq files it contains are not properly named. Input demultiplexed fastq files must end with '.fastq.gz'."
+        echo -e "\nExiting.\n"
+        exit 1
+      fi
+    fi
+  elif [[ ! -f "$FASTQ" ]] ; then
+    echo -e "\nERROR: The input fastq file ${FASTQ} provided in the config file does not exist. Please make sure the file exists and its path is correct."
+    echo -e "\nExiting.\n"
+    exit 1
+  elif [[ "$FASTQ" != *.fastq.gz ]] ; then
+      echo -e "\nERROR: Provided FASTQ (${FASTQ}) is not properly named. Multiplexed fastq files must end with .fastq.gz"
+      echo -e "\nExiting.\n"
+      exit 1
+  fi
+fi
+
+# DEMULT_THREADS and DEMULT_SUBSTITUTIONS
+DEMULT_THREADS=$(grep "^DEMULT_THREADS:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+DEMULT_SUBSTITUTIONS=$(grep "^DEMULT_SUBSTITUTIONS:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+if [[ -z "$DEMULT_DIR" && -z "$DEMULT_THREADS" ]] ; then
+  echo -e "\nERROR: A DEMULT_THREADS value must be provided in the config file."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+
+
+if [[ -z "$DEMULT_DIR" && -z "$DEMULT_SUBSTITUTIONS" ]] ; then
+  echo -e "\nERROR: A DEMULT_SUBSTITUTIONS value must be provided in the config file."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+
+
+# BARCODE_FILE and ADAPT_FILE
+BARCODE_FILE=$(grep "^BARCODE_FILE:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+ADAPT_FILE=$(grep "^ADAPT_FILE:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+BARCODE_FILE=$(absolutePath $BARCODE_FILE)
+ADAPT_FILE=$(absolutePath $ADAPT_FILE)
+
+if [[ -z "$DEMULT_DIR" && -z "$BARCODE_FILE" ]] ; then
+  echo -e "\nERROR: A BARCODE_FILE must be provided in the config file."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+if [[ ! -z "$BARCODE_FILE" && ! -f "$BARCODE_FILE" ]] ; then
+  echo -e "\nERROR: The BARCODE_FILE provided in the config file (${BARCODE_FILE}) does not exist. Please make sure the file exists and its path is correct."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+
+if [[ ! -z "$BARCODE_FILE" ]] ; then
+  nb_carriage_returns=$(grep -c $'\r' $BARCODE_FILE)
+  if [[ "$nb_carriage_returns" -gt 0 ]] ; then
+    echo "Removing windows carriage returns in ${BARCODE_FILE}..."
+    sed -i 's/\r$//g' $BARCODE_FILE
+    sed -i 's/\r/\n/g' $BARCODE_FILE
+  fi
+  nb_col_barcode_file=$(awk -F '\t' '{print NF}' $BARCODE_FILE | sort | uniq)
+  if [[ "$WORKFLOW_SMK" = "${WORKFLOW}_PairedEnd.smk" && $nb_col_barcode_file != 3 ]] ; then
+    echo -e "\nERROR: The barcode file (${BARCODE_FILE}) provided in the config file does not have a proper format. For paired end data, 3 columns are expected, separated by tabs."
+    echo -e "\nExiting.\n"
+    exit 1
+  elif [[ "$WORKFLOW_SMK" = "${WORKFLOW}_SingleEnd.smk" && $nb_col_barcode_file != 2 ]] ; then
+    echo -e "\nERROR: The barcode file (${BARCODE_FILE}) provided in the config file does not have a proper format. For single end data, 2 columns are expected, separated by tabs."
+    echo -e "\nExiting.\n"
+    exit 1
+  fi
+fi
+
+
+if [[ -z "$ADAPT_FILE" ]] ; then
+  echo -e "\nERROR: An ADAPT_FILE must be provided in the config file."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+if [[ ! -z "$ADAPT_FILE" && ! -f "$ADAPT_FILE" ]] ; then
+  echo -e "\nERROR: The ADAPT_FILE provided in the config file (${ADAPT_FILE}) does not exist. Please make sure the file exists and its path is correct."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+nb_carriage_returns=$(grep -c $'\r' $ADAPT_FILE)
+if [[ "$nb_carriage_returns" -gt 0 ]] ; then
+  echo "Removing windows carriage returns in ${ADAPT_FILE}..."
+  sed -i 's/\r$//g' $ADAPT_FILE
+  sed -i 's/\r/\n/g' $ADAPT_FILE
+fi
+
+nb_col_adapt_file=$(awk -F '\t' '{print NF}' $ADAPT_FILE | sort | uniq)
+if [[ "$WORKFLOW_SMK" = "${WORKFLOW}_PairedEnd.smk" && $nb_col_adapt_file != 3 ]] ; then
+  echo -e "\nERROR: The adapter file (${ADAPT_FILE}) provided in the config file does not have a proper format. For paired end data, 3 columns are expected, separated by tabs."
+  echo -e "\nExiting.\n"
+  exit 1
+elif [[ "$WORKFLOW_SMK" = "${WORKFLOW}_SingleEnd.smk" && $nb_col_adapt_file != 2 ]] ; then
+  echo -e "\nERROR: The adapter file (${ADAPT_FILE}) provided in the config file does not have a proper format. For single end data, 2 columns are expected, separated by tabs."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+
+# TRIMMING_THREADS, TRIMMING_QUAL and TRIMMING_MIN_LENGTH
+TRIMMING_THREADS=$(grep "^TRIMMING_THREADS:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+if [[ -z $TRIMMING_THREADS ]] ; then
+  echo -e "\nERROR: You must provide the TRIMMING_THREADS in the config_file."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+
+TRIMMING_QUAL=$(grep "^TRIMMING_QUAL:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+if [[ -z $TRIMMING_QUAL ]] ; then
+  echo -e "\nERROR: You must provide the TRIMMING_QUAL in the config_file."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
+
+TRIMMING_MIN_LENGTH=$(grep "^TRIMMING_MIN_LENGTH:" $CONFIG | sed 's/#.*$//' | cut -d ' ' -f2 | sed 's/"//g')
+if [[ -z $TRIMMING_MIN_LENGTH ]] ; then
+  echo -e "\nERROR: You must provide the TRIMMING_MIN_LENGTH in the config_file."
+  echo -e "\nExiting.\n"
+  exit 1
+fi
--- DATA_CLEANING/WORKFLOW/SCRIPTS/launcher_basicCheck.sh
+++ DATA_CLEANING/WORKFLOW/SCRIPTS/launcher_basicCheck.sh
@@ -3,11 +3,8 @@
 HERE=$PWD
 
 ### 0/ Variables help
-workflow_path_help=""
-workflow_help=""
-cluster_config_help=""
-config_file_help=""
-
+workflow_help=$(grep -e "^--workflow " ${WORKFLOW_PATH}/SCRIPTS/launcher_help.txt)
+config_file_help=$(grep -e "^--config-file " ${WORKFLOW_PATH}/SCRIPTS/launcher_help.txt)
 
 
 ### 1/ WORKFLOW  ###
@@ -30,45 +27,16 @@ if [[ -z $(echo $available_workflows | grep -w $WORKFLOW) ]] ; then
 fi
 
 
-### 2/ CLUSTER_CONFIG and CONFIG_FILE ###
+### 2/ CONFIG_FILE ###
 
-## CLUSTER_CONFIG ##
-# In case it was not provided
-if [[ -z "$CLUSTER_CONFIG" || "$CLUSTER_CONFIG" = --* || "$CLUSTER_CONFIG" = -* ]] ; then
-  if [[ -f "CONFIG/cluster_config_${WORKFLOW}.json" ]] ; then
-    CLUSTER_CONFIG=CONFIG/cluster_config_${WORKFLOW}.json
-    echo -e "\nThe cluster_config_${WORKFLOW}.json file was automatically found in ${HERE}/CONFIG and will be used as the cluster_config file for this workflow."
-    echo -e "If you would rather use another cluster_config file, please provide it with --cluster-config\n"
-  else
-    echo -e "\nERROR: The cluster config file cannot be found. Please provide your cluster config file with --cluster-config or place it in a CONFIG folder under the name cluster_config_${WORKFLOW}.json."
-		echo "As a reminder:"
-		echo $cluster_config_help
-		echo -e "\nExiting.\n"
-    exit 1
-  fi
-else # In case it was provided
-  if [[ ! -f "$CLUSTER_CONFIG" ]] ; then
-    echo -e "\nERROR: the file given in the --cluster-config parameter is not valid. Please make sure the file exists and the path is correctly written."
-		echo "As a reminder:"
-		echo $cluster_config_help
-		echo -e "\nExiting.\n"
-    exit 1
-  fi
-fi
-# Absolute path
-if [[ ! "$CLUSTER_CONFIG" = /* ]] ; then
-  CLUSTER_CONFIG=$(readlink -f $CLUSTER_CONFIG) ;
-fi
-# Remove potential \r from file
-sed -i 's/\r$//g' $CLUSTER_CONFIG
-sed -i 's/\r/\n/g' $CLUSTER_CONFIG
+CONFIG=$(absolutePath $CONFIG)
 
-## CONFIG ##
 # In case it was not provided
 if [[ -z "$CONFIG" || "$CONFIG" = --* || "$CONFIG" = -* ]] ; then
   if [[ -f "CONFIG/config_${WORKFLOW}.yml" ]] ; then
     CONFIG=CONFIG/config_${WORKFLOW}.yml
-    echo -e "\nThe config_${WORKFLOW}.yml file was automatically found in ${HERE}/CONFIG and will be used as the config file for this workflow."
+		CONFIG=$(absolutePath $CONFIG)
+    echo -e "\nINFO: The config_${WORKFLOW}.yml file was automatically found in ${HERE}/CONFIG and will be used as the config file for this workflow."
     echo -e "If you would rather use another config file, please provide it with --config-file\n"
   else
     echo -e "\nERROR: The workflow config file cannot be found. Please provide your config file with --config-file or place it in a CONFIG folder under the name config_${WORKFLOW}.yml."
@@ -76,25 +44,41 @@ if [[ -z "$CONFIG" || "$CONFIG" = --* || "$CONFIG" = -* ]] ; then
 		echo $config_file_help
 		echo -e "\nExiting.\n"
     exit 1
-  fi
-else # In case it was provided
-  if [[ ! -f "$CONFIG" ]] ; then
+  fi # In case it was provided
+elif [[ ! -f "$CONFIG" ]] ; then
     echo -e "\nERROR: the file given in the --config-file parameter is not valid. Please make sure the file exists and the path is correctly written."
 		echo "As a reminder:"
 		echo $config_file_help
 		echo -e "\nExiting.\n"
     exit 1
-  fi
 fi
-# Absolute path
-if [[ ! "$CONFIG" = /* ]] ; then
-  CONFIG=$(readlink -f $CONFIG) ;
+
+nb_spaces=$(grep -c '  .*' $CONFIG)
+if [[ "$nb_spaces" -gt 0 ]] ; then
+	echo "Removing extra spaces in ${CONFIG}..."
+  sed -i 's/  */ /g' $CONFIG
 fi
-# Remove potential \r from file
-sed -i 's/\r$//g' $CONFIG
-sed -i 's/\r/\n/g' $CONFIG
 
 
+nb_carriage_returns=$(grep -c $'\r' $CONFIG)
+if [[ "$nb_carriage_returns" -gt 0 ]] ; then
+	echo "Removing windows carriage returns in ${CONFIG}..."
+  sed -i 's/\r$//g' $CONFIG
+  sed -i 's/\r/\n/g' $CONFIG
+fi
+
+
+# Format
+nb_col_config_file=$(grep -v '^#' $CONFIG | sed 's/#.*$//' | grep -v '^[[:space:]]*$' | awk -F ': ' '{print NF}' | sort | uniq)
+nb_tabs_config_file=$(grep -v '^#' $CONFIG | sed 's/#.*$//' | grep -v '^[[:space:]]*$' | grep -c $'\t')
+if [[ "$nb_col_config_file" != 2 || $nb_tabs_config_file -gt 0 ]] ; then
+  echo -e "\nERROR: Your config file (${CONFIG}) does not seem to be properly formated."
+  echo "As a reminder:"
+  echo "Variables must be specified in the yaml format, with one variable per row, followed by a ':' and a space before the assigned value, for example : VARIABLE_NAME: value."
+  echo "Some variables can be left empty, for example: VARIABLE_NAME: \"\" "
+  echo -e "\nExiting.\n"
+  exit 1
+fi
 
 
 
@@ -117,12 +101,26 @@ if [[ "$WORKFLOW" = "DataCleaning" ]] ; then
 	fi
 fi
 
-# if the smk file doesn't exist
-
 if [[ ! -f "${WORKFLOW_PATH}/${WORKFLOW_SMK}" ]] ; then
 	echo -e "\nERROR: The workflow name provided with --workflow is incorrect."
 	echo "As a reminder:"
 	echo $workflow_help
 	echo -e "\nExiting.\n"
 	exit 1
+fi
+
+nb_carriage_returns=$(grep -c $'\r' ${WORKFLOW_PATH}/$WORKFLOW_SMK)
+if [[ "$nb_carriage_returns" -gt 0 ]] ; then
+	echo "Removing windows carriage returns in ${WORKFLOW_SMK}..."
+	sed -i 's/\r$//g' ${WORKFLOW_PATH}/$WORKFLOW_SMK
+	sed -i 's/\r/\n/g' ${WORKFLOW_PATH}/$WORKFLOW_SMK
+fi
+
+### 4/ Check if Snakemake module is available
+if ! [[ -x "$(command -v snakemake)" ]] ; then
+  echo -e "\nERROR: Snakemake is not available. You must install it, or make it available to your working environment (eg: module load it or activate it with conda)."
+  echo "As a reminder:"
+  awk '/^- Make sure Snakemake and Conda/,/^$/' ${WORKFLOW_PATH}/SCRIPTS/launcher_help.txt
+  echo -e "\nExiting.\n"
+  exit 1
 fi
