f7ac690e2b1a38177bf47e1f4821b39d40f74826
ArdissonM <65232720+ArdissonM@users.noreply.github.com>
2022-03-18 21:57:56 +0100

    SNP_CALLING valided

--- SNP_CALLING/EXAMPLE/snpcaller.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-module purge
-module load snakemake/5.13.0
-module load anaconda/python3.8
-
-snakemake --snakefile /home/ardissonm/replicated/WORKFLOW/SNP_CALLING/GATK_ENV.smk --use-conda --jobs 1 
-#--configfile CONFIG/config_SnpCaller.yml
--- SNP_CALLING/WORKFLOW/GATK_ENV.smk
+++
@@ -1,9 +0,0 @@
-rule all:
-    input:
-    output:
-        "res.txt"
-    params:
-    conda:
-        "ENVS/conda_tools.yml"
-    shell:
-        "gatk --help > res.txt"
