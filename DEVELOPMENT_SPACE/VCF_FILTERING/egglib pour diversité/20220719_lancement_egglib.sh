d95444d627cc84570e9333452afc25416c4debdc
ArdissonM <65232720+ArdissonM@users.noreply.github.com>
2022-08-04 18:30:41 +0200

    DEVELOPMENT_SPACE

--- DEVELOPMENT_SPACE/VCF_FILTERING/egglib pour diversité/20220719_lancement_egglib.sh
+++ DEVELOPMENT_SPACE/VCF_FILTERING/egglib pour diversité/20220719_lancement_egglib.sh
@@ -8,12 +8,6 @@ cd /home/ardissonm/scratch/DEV/ANALYSES_2022_WORKFLOW/DEV_Cap009_and_Cap010/WORK
 egglib_vcf_stats.py
 labels_groups.txt
 
-python egglib_vcf_stats.py
-
-
-
-# avec egglib_vcf_stats2.py
-
 
 python egglib_vcf_stats.py --bed_file /home/ardissonm/scratch/DEV/ANALYSES_2022_WORKFLOW/DEV_Cap009_and_Cap010/WORKFLOWS_OUTPUTS/READS_MAPPING/MAPPING_ZAVITAN_WITH_DUPLICATES/BAMS/all_covered_zones_119_extra100pb_collapsed_min100.bed --vcf_file 03_PopGenStatsSampleLocus_Filtered.vcf --labels_file labels_groups.txt
 
