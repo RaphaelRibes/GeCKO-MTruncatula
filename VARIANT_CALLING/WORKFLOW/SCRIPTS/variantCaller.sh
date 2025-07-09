58e6bb4e1bbf61efbc9aeaaa045aacbe15b69a9f
raphael <raphael.ribes@gmail.com>
2025-07-09 12:22:57 +0200

    idk why but parabricks deepvariant generates vcf files too if you want gvcf so I delete them

--- VARIANT_CALLING/WORKFLOW/SCRIPTS/variantCaller.sh
+++ VARIANT_CALLING/WORKFLOW/SCRIPTS/variantCaller.sh
@@ -22,11 +22,14 @@ pbrun deepvariant \
 echo ""
 echo "DeepVariant completed successfully."
 
+# Remove the unwanted .vcf.gz file if it exists
+BASENAME=$(basename "$3" .g.vcf.gz)
+DIRNAME=$(dirname "$3")
+rm -f "${DIRNAME}/${BASENAME}.vcf.gz"
+
 # Index the output GVCF file
 echo "Indexing GVCF file: $3"
 pbrun indexgvcf \
     --input "$3"
 echo ""
 echo "Indexing of GVCF completed successfully."
-
-# singularity exec utils/singularity_image/parabricks.sif ./VARIANT_CALLING/WORKFLOW/SCRIPTS/variantCaller.sh /lustre/ribesr/genom-asm-4-pg-haploid/results/ESP099_results/02_final_assembly/hap/ragtag_scafold/sorted/M.truncatula.fa /lustre/ribesr/GeCKO-MTruncatula/WORKFLOWS_OUTPUTS/READ_MAPPING/BAMS /lustre/ribesr/GeCKO-MTruncatula/test shortread
