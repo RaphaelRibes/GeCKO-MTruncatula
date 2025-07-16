f0ef7ff09d19f3ab800e76dda5250b73a5ac6172
raphael <raphael.ribes@gmail.com>
2025-07-16 13:37:00 +0200

    ~VARIANT_CALLING -> GLNexus: remove GLnexus.DB after it being used

--- VARIANT_CALLING/WORKFLOW/SCRIPTS/GLNexus.sh
+++ VARIANT_CALLING/WORKFLOW/SCRIPTS/GLNexus.sh
@@ -2,9 +2,9 @@
 
 ulimit -Sn 65536
 
-mkdir -p "$1";
 # glnexus_cli is the executable within the GLnexus container
 glnexus_cli --list "$2" --config "$3" --thread "$4" > "$1"/joint_calls.bcf
 
-bcftools convert -O z -o "$1"/joint_calls.vcf.gz ."$1"/joint_calls.bcf
+bcftools convert -O z -o "$1"/joint_calls.vcf.gz "$1"/joint_calls.bcf
 bcftools index -t "$1"/joint_calls.vcf.gz
+rm -rf ./GLnexus.DB
